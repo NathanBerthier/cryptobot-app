6c25ed63511ebf8dc0b0d158bee000466c3115fd
Nathan Berthier <nathanberthier@outlook.fr>
2021-10-18 14:41:19 +0200

    Refacto job

--- app/jobs/moula_job.rb
+++ app/jobs/moula_job.rb
@@ -17,8 +17,7 @@ class MoulaJob < ApplicationJob
     balance_eth     = 0
     up_count        = 0
 
-    loop do # rubocop:disable Block/BlockLength
-      puts 'Waiting...'
+    loop do
       sleep(5)
       eth_price = eth.price
       dai_price = dai.price
@@ -27,37 +26,16 @@ class MoulaJob < ApplicationJob
       if can_buy?(direction, balance_dai) && up_count == 1
         balance_eth = eth.buy(balance_dai, dai_price, eth_price)
         balance_dai = 0
-        buy_price = eth_price
-        buy_at = DateTime.now.strftime('%I:%M:%S')
         up_count = 0
       elsif can_buy?(direction, balance_dai) && up_count != 1 && eth_price != reference_price
         up_count += 1
       elsif can_sell?(direction, balance_dai) && eth_price != reference_price
         balance_dai = eth.sell(balance_eth, eth_price, dai_price)
         balance_eth = 0
-        sell_price = eth_price
-        sell_at = DateTime.now.strftime('%I:%M:%S')
         up_count = 0
       elsif direction.negative? && balance_dai.positive? && eth_price != reference_price
         up_count = 0
       end
-
-      puts "Reference price: #{reference_price}"
-      puts "Current price: #{eth_price}"
-      if direction.positive?
-        puts 'Direction: 📈'
-      else
-        puts 'Direction: 📉'
-      end
-      puts '-------------------------------------'
-      puts "Buy price: #{buy_price} | #{buy_at}"
-      puts "Sell price: #{sell_price} | #{sell_at}"
-      puts '-------------------------------------'
-      puts "DAI balance: #{balance_dai} DAI"
-      puts "DAI balance: #{balance_dai * dai_price} $"
-      puts "ETH balance: #{balance_eth} ETH"
-      puts "ETH balance: #{balance_eth * eth_price} $"
-      puts "at #{DateTime.now.strftime('%I:%M:%S')}"
 
       reference_price = eth_price
       break if read_bob_status == 'waiting'
